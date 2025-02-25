0e7eb04b796fd4a7ecb325f07a61d75dade91b93
Praveen Kumar <praveenjillu17@gmail.com>
2025-02-25 12:21:42 +0530

    Add files via upload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+	selector: 'app-header',
+	standalone: true,
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	header = 'Accenture Employee Data';
+	desc = 'Enterprise level task management without friction';
+}
